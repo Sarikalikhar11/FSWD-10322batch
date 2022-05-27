2e20a1a62372cbf81c98a9740f8fe16373b6c378
Sarika Likhar <sarikalikhar2619@gmail.com>
2022-05-27 18:10:07 +0530

    commit promises

--- app1.js
+++ app1.js
@@ -1,18 +1,46 @@
-const getSum = (arr=[])=>new Promise((resolve, reject)=>{
-    if(arr.length==0) {reject('Invalid array')}
-    else{
-        const sum = arr.reduce((a,b)=>{
-            console.log({a,b})
-            return a + b
-        })
-        resolve(sum)
-    }
+const endpoint = "https://jsonplaceholder.typicode.com/todos";
+
+// const getSum = (arr=[])=>new Promise((resolve, reject)=>{
+//     if(arr.length==0) {reject('Invalid array')}
+//     else{
+//         const sum = arr.reduce((a,b)=>{
+//             console.log({a,b})
+//             return a + b
+//         })
+//         resolve(sum)
+//     }
      
-})
+// })
 
-const arr=[1,2,3,4,5,6,7];
+// const arr=[1,2,3,4,5,6,7];
 
-getSum(arr).then(sum=>{
-    console.log(sum);
-})
+// getSum(arr).then(sum=>{
+//     console.log(sum);
+// })
+
+const button = document.querySelector('button');
+const list = document.querySelector('ol');
+console.log(button,list);
+
+const fetchData = ()=>{
+    fetch(endpoint)
+    .then(stream=>stream.json())
+    .then(todos=>{
+            todos.map(todo=>{
+            const listelement = document.createElement('li');
+            listelement.innerHTML = todo.title;
+            const isCompleted = todo.completed;
+            if(isCompleted){
+                listelement.style = "color:crimson"
+            }else{
+                listelement.style = "color:lime"
+            }
+            console.log(listelement);
+            list.appendChild(listelement);
+        })
+    })
+}
+button.addEventListener('click',fetchData)
+
+
 
